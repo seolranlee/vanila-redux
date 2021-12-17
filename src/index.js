f0b61939d13cec9c94410474c9b7505970908a4b
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2021-12-17 18:26:35 +0900

    #2.2 Delete To Do

--- src/index.js
+++ src/index.js
@@ -7,13 +7,29 @@ const ul = document.querySelector("ul")
 const ADD_TODO = "ADD_TODO"
 const DELETE_TODO = "DELETE_TODO"
 
+
+// action creator
+const addToDo = text => {
+  return {
+    type: ADD_TODO, 
+    text
+  }
+}
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
+
 const reducer = (state = [], action) => {
   console.log(action)
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }]
+      return [{ text: action.text, id: Date.now() }, ...state]
     case DELETE_TODO:
-      return []
+      return state.filter((todo) => todo.id !== action.id)
     default:
       return state
   }
@@ -23,11 +39,37 @@ const store = createStore(reducer)
 
 store.subscribe(() => console.log(store.getState()))
 
+const paintToDos = () => {
+  const toDos = store.getState()
+  ul.innerHTML = ""
+  toDos.forEach(toDo => {
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+    li.id = toDo.id
+    li.innerText = toDo.text
+    btn.innerText = "DEL"
+    btn.addEventListener("click", dispatchDeleteToDo)
+    li.appendChild(btn)
+    ul.appendChild(li)
+  })
+}
+
+store.subscribe(paintToDos)
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text))
+} 
+
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id
+  store.dispatch(deleteToDo(id))
+}
+
 const onSubmit = (e) => {
   e.preventDefault()
   const toDo = input.value
   input.value = ""
-  store.dispatch({ type: ADD_TODO, text: toDo })
+  dispatchAddToDo(toDo)
 }
 
 form.addEventListener("submit", onSubmit)
