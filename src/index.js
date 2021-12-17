d09ad33a2ddaac92d1720ecc4aeae023a3000ea0
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2021-12-17 16:54:20 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
+import { createStore } from "redux"
+
 const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
-let count = 0
 
-number.innerText = count
-
-const updateText = () => {
-  number.innerText = count
+// reducer는 data를 modify하는 함수이다.
+const countModifier = (count = 0) => {
+  return count
 }
 
-const handleAdd = () => {
-  count = count + 1
-  updateText()
-}
+const countStore = createStore(countModifier)
 
-const handleMinus = () => {
-  count = count - 1
-  updateText()
-}
-
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
+console.log(countStore.getState())
