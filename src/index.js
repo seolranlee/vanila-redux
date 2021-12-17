24e3ead9c52ee266e09e5a1a49823025d7fcdc46
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2021-12-17 17:22:15 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
+number.innerText = 0
 
 // reducer는 data를 modify하는 함수이다.
 const countModifier = (count = 0, action) => {
-  console.log(count, action)
   if (action.type === "ADD") {
     return count + 1
   } else if (action.type === 'MINUS') {
@@ -19,11 +19,20 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier)
 
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "ADD" })
-countStore.dispatch({ type: "MINUS" })
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
 
-console.log(countStore.getState())
+// state의 변화를 감지.
+countStore.subscribe(onChange)
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" })
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" })
+}
+
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)
